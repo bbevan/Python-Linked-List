75ab9f1f3e692cea1e0a2989da2ee8c414b1bfde
brandon <nbbevan@gmail.com>
2020-01-12 09:36:56 -0500

    better iterator design

--- LinkedList.py
+++ LinkedList.py
@@ -2,46 +2,69 @@
 class Node:
     
     def __init__(self, value):
-        self.node = value
-        self.next = None
+        self.node = value # the node itself
+        self.next = None  # pointer to the next node
    
 class LinkedList:
 
     def __init__(self,value):
-        self.first = Node(value)
-        self.current = self.first
+        self.first = Node(value)  # first element of the list is a node
+        self.current = self.first # pointer to the current node
         
 
     def addNode(self, value):
-        self.current.next = Node(value)
-        self.moveForward()
+        self.current.next = Node(value) # let the pointer become a node
+        self.moveForward() 
 
     def moveForward(self):
-        self.current = self.current.next
+        self.current = self.current.next # move the pointer forward
 
     def firstNode(self):
-        self.current = self.first
+        self.current = self.first # go back to the first node
 
     def getNode(self):
-        return [self.label, self.current]
+        return self.current.node # get the value of the node
 
     def getNext(self):
-        return self.current.next
+        return self.current.next # get pointer to the next node
+                                 # `current` is always a pointer!
 
+    def isDone(self):
+        if (self.current != None):
+            pass
+        else:
+            return True
+
+    # print the entire linked list
     def printList(self):
         self.firstNode()
-        while (self.current != None):
-            print(self.current.node)
+        
+        while (not self.isDone()):
+            print(self.getNode())
             self.moveForward()
 
+    # print the current node
     def printNode(self):
         print(self.current.node)
 
+
 class LabeledLinkedList(LinkedList):
 
+    # Extends LinkedList nodes with labels
+    
     def __init__(self, value, label):
-        self.label = label
+        self.firstlabel = label
+        self.currentlabel = self.firstlabel
         super().__init__(value)
+
+    def addNode(self,value,label):
+        self.current.nextlabel = label
+        self.moveForward()
+        super().addNode(value)
+
+    def moveForward(self):
+        self.currentlabel = self.current.nextlabel
+
 
     
 
@@ -50,5 +73,4 @@ myList.addNode("b")
 myList.printList()
 
 myLabeled = LabeledLinkedList(1, 1)
-print(myLabeled.first.node)
-print(myLabeled.label)
+myLabeled.addNode(2, 2)
